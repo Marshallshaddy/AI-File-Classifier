a04f4e18b9229e7287b5aec68097601be388ce63
Marshallshaddy <marshallshaddy6@gmail.com>
2024-12-24 23:08:09 +0100

    Shuffled AIFC Dataset
    Created a Program for shuffling the Dataset. AIFC Dataset was shuffled to seperate the data into training and tests set and to remove any potential ordering bias in the data

---
+++ Dataset Creation Program/Dataset_Shuffling.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+import numpy as np
+
+# Load the dataset
+file_path = "Data\AIFC_Dataset.csv"
+df = pd.read_csv(file_path)
+
+# Shuffle the dataset
+shuffled_df = df.sample(frac=1, random_state=None)
+
+# Save the shuffled dataset to a new CSV file
+shuffled_file_path = "Data\Shuffled_AIFC_Dataset.csv"
+shuffled_df.to_csv(shuffled_file_path, index=False)
+
+print('Shuffled dataset created and saved to:', shuffled_file_path)
